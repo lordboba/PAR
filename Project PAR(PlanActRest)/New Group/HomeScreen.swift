0ff30f2c553679ab844ca1fd9eb8bd16b1f582bd
Tyler Xiao <yxiao1717@gmail.com>
2022-12-20 10:31:39 -0800

    12/19 stuff

---
+++ Project PAR(PlanActRest)/New Group/HomeScreen.swift
@@ -0,0 +1,33 @@
+//
+//  HomeScreen.swift
+//  Project PAR(PlanActRest)
+//
+//  Created by Tyler Alex on 12/19/22.
+//
+
+import UIKit
+
+class HomeScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let hex:UInt64 = 0xA0E4CB
+        let r = (hex & 0xff0000) >> 16
+        let g = (hex & 0xff00) >> 8
+        let b = hex & 0xff
+        view.backgroundColor = UIColor(red: CGFloat(r) / 256.0, green: CGFloat(g) / 256.0, blue: CGFloat(b) / 256.0, alpha: 1)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Project PAR(PlanActRest)/ViewController.swift
+++ Project PAR(PlanActRest)/ViewController.swift
@@ -12,8 +12,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//        let hex:UInt64 = 0xA0E4CB
+//        let r = (hex & 0xff0000) >> 16
+//        let g = (hex & 0xff00) >> 8
+//        let b = hex & 0xff
+//        view.backgroundColor = UIColor(red: CGFloat(r) / 256.0, green: CGFloat(g) / 256.0, blue: CGFloat(b) / 256.0, alpha: 1)
+
+
     }
 
-
 }
-
