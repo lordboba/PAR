c74e2c517de402c6616bda89a62a81858d57fba3
Tyler Xiao <yxiao1717@gmail.com>
2022-12-29 14:26:33 -0800

    someReflectChanges

---
+++ Project PAR(PlanActRest)/New Group/Reflection.swift
@@ -0,0 +1,35 @@
+//
+//  Reflection.swift
+//  Project PAR(PlanActRest)
+//
+//  Created by Tyler Xiao on 12/29/22.
+//
+
+import UIKit
+
+class Reflection: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet var sliderResults: UISlider!
+    
+    @IBAction func showTips(_ sender: Any) {
+    }
+    
+    @IBAction func toBreak(_ sender: Any) {
+        //save reflection results to database/statistics
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
