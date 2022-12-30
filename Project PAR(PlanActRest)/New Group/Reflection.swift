a5a0b4c15bcbb1e945715eb0424ef4df0625fff7
emma shen <aimashen.2017@gmail.com>
2022-12-29 17:22:03 -0800

    randomly generated tips

--- Project PAR(PlanActRest)/New Group/Reflection.swift
+++ Project PAR(PlanActRest)/New Group/Reflection.swift
@@ -18,7 +18,7 @@ class Reflection: UIViewController {
     
     @IBOutlet var tipView: UIView!
     @IBOutlet var tipText: UILabel!
-    var tips = ["eat salad", "eat pizza"]
+    var tips = ["If you are feeling overwhelmed, focus on one task at a time! You got this!", "Eat the rainbow! Vegetables and fruits keep your body healthy and your mind sharp.", "Make a to-do-list in the morning. Clear your head and get ready to tackle what the day brings!", "Exercise at least 30 minutes everyday! Here are some easy ways to get started: take a walk around the neighborhood, swim at the beach, play your favorite sport with your friends.", "In the mood for some fun games to exercise your brain? Try these board games: Catan, Apple to Apples, Scrabble, Monopoly, and Chess.", "Eat for the environment. You can fight climate change by eating less packaged foods, animal meats, and growing your own food!", "Feeling tired and unmotivated? A good 8 hours of sleep every night can ensure an energized and fresh mind and body in the morning!.", "Getting distracted by noise around you? Consider changing your work space to somewhere more quiet. For example, a secluded room or the library."]
     @IBAction func exitFromTip(_ sender: Any) {
         animateOut(desiredView: tipView)
     }
