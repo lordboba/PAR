f999c4156e81e9407c232564afe5fce3c8b7ef96
emma shen <aimashen.2017@gmail.com>
2023-01-07 16:53:36 -0800

    tutorial pop up

--- Project PAR(PlanActRest)/New Group/HomeScreen.swift
+++ Project PAR(PlanActRest)/New Group/HomeScreen.swift
@@ -11,6 +11,7 @@ class HomeScreen: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        animateIn(desiredView: welcomePop)
         let hex:UInt64 = 0xA0E4CB
         let r = (hex & 0xff0000) >> 16
         let g = (hex & 0xff00) >> 8
@@ -22,10 +23,45 @@ class HomeScreen: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    
+    
     @IBAction func TutorialButton(_ sender: Any) {
         print("hi")
         //sender.font = "Times New Roman"
     }
+    
+    @IBOutlet var welcomePop: UIView!
+    
+    
+    @IBAction func pressStartB(_ sender: Any) {
+        animateOut(desiredView: welcomePop)
+    }
+    
+    func animateIn(desiredView: UIView) {
+        let backgroundView = self.view!
+        backgroundView.addSubview(desiredView)
+        
+        desiredView.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+        desiredView.alpha = 0
+        desiredView.center = backgroundView.center
+        
+        UIView.animate(withDuration: 0.3, animations: {
+            
+            desiredView.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
+            desiredView.alpha = 1
+            
+        })
+        
+    }
+    func animateOut(desiredView: UIView) {
+        UIView.animate(withDuration: 0.3, animations: {
+            desiredView.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+            desiredView.alpha = 0
+        }, completion: { _ in
+            desiredView.removeFromSuperview()
+        })
+    }
+    
     
     /*
     // MARK: - Navigation
